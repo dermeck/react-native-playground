92b61dd99304a8562c662d952c3d4215e90aef07
Martin Runge <martinrunge@posteo.de>
2016-08-21 16:39:28 +0200

    added first styles

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -1,17 +1,26 @@
 // Import some Code we need
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
 // Create a react component
 var Weekdays = React.createClass({
     render: function() {
-        return <View>
+        return <View style={styles.container}>
             <Text>
                 Hello World!
             </Text>
         </View>
     }
 });
+
+// Style
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 // Show the react component on the screen => entry point of the app
 AppRegistry.registerComponent('weekdays', function() {
