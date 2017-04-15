890f4b29a81edcf4afab9dec5854ab3f8b8a13a0
Martin Runge <martinrunge@posteo.de>
2017-04-15 13:14:23 +0200

    basic app

---
+++ albums/index.ios.js
@@ -0,0 +1,15 @@
+// code for IOS
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+// Create a component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
