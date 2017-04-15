3e618be6a19eaf0884134868b06ef73882767cd5
Martin Runge <martinrunge@posteo.de>
2017-04-15 17:33:41 +0200

    added basic header component

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -3,11 +3,12 @@
 // Import a library to help create a component
 import React from 'react';
 import { AppRegistry, Text } from 'react-native';
+import { Header } from './src/components/Header';
 
 // Create a component
 const App = () => {
   return (
-    <Text>Some Text</Text>
+    <Header />
   );
 };
 
