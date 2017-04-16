4e41751111f78f888ec4a2c12d36806fa40a30e5
Martin Runge <martinrunge@posteo.de>
2017-04-16 17:05:23 +0200

    list album titles

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,15 +3,25 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 export class AlbumList extends Component {
+    // default state
+    state = { albums: []};
+
     componentWillMount(){
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response));
+            .then(response => this.setState({ albums: response.data}));
+    }
+
+    renderAlbums(){
+        return this.state.albums.map(album =>
+            <Text key={album.title}>{album.title}</Text>);
     }
 
     render() {
+        console.log(this.state);
+
         return (
             <View>
-                <Text>AlbumList</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
