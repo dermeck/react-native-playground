24ba5085136b75f1a46b7cbba4acacf13339e258
Martin Runge <martinrunge@posteo.de>
2017-04-16 10:04:17 +0200

    pass header text as prop

--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-export const Header = () => {
+export const Header = (props) => {
     const { textStyle, viewStyle } = styles;
 
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums!</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
