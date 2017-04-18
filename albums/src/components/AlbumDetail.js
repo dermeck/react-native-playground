1a364e003bcc29d0ababeb8c919a8cf6636927fb
Martin Runge <martinrunge@posteo.de>
2017-04-18 11:00:51 +0200

    additional styling of entry in albumdetails

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Text, Image, View } from 'react-native';
-import Card from './Card';
-import CardSection from './CardSection';
+import Card from './Card'
+import CardSection from './CardSection'
 
-export default AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image} = album;
+export default AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image, image} = album
+  const {
+    thumbnailStyle,
+    headerTextStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={ thumbnailContainerStyle }>
           <Image
-            style={styles.thumbNailStyle}
-            source={{ uri: thumbnail_image }}
+            style={thumbnailStyle}
+            source={{uri: thumbnail_image}}
           />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>
             {title}
           </Text>
           <Text>
             {artist}
           </Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{uri: image}}/>
       </CardSection>
     </Card>
   )
@@ -33,8 +43,22 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
-  thumbNailStyle: {
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
