bb411f0b91fd1550718a450f2cb093c459258ca2
Martin Runge <martinrunge@posteo.de>
2017-04-23 14:54:35 +0200

    created basic LoginForm component

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
 import config from './config/firebase';
+import LoginForm from './components/LoginForm'
 
 export default class App extends Component {
     componentWillMount(){
@@ -13,6 +14,7 @@ export default class App extends Component {
         return (
           <View>
               <Header headerText="Auth!" />
+              <LoginForm/>
           </View>
         );
     }
