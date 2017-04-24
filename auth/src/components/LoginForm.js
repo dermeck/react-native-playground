4b56135021318e660fbf42ba9a930a40e83132df
Martin Runge <martinrunge@posteo.de>
2017-04-24 20:36:57 +0200

    added TextInput to LoginForm

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+    state = {text: ''};
+
+
     render() {
         return (
-          <Card>
-              <CardSection></CardSection>
-              <CardSection></CardSection>
-              <CardSection>
-                  <Button>
-                      Login!
-                  </Button>
-              </CardSection>
-          </Card>
+            <Card>
+                <CardSection>
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={text => this.setState({text})}
+                        style={{height: 20, width: 100}}
+                    />
+                </CardSection>
+                <CardSection></CardSection>
+                <CardSection>
+                    <Button>
+                        Login!
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
