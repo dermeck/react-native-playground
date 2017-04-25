ef3fda393a8d5e15af6864e701cb1e5bcbb7993d
Martin Runge <martinrunge@posteo.de>
2017-04-25 19:46:32 +0200

    added password field

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = {email: ''};
+    state = {email: '', password: ''};
 
     render() {
         return (
@@ -15,7 +15,15 @@ class LoginForm extends Component {
                         onChangeText={text => this.setState({email: text })}
                     />
                 </CardSection>
-                <CardSection></CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        placeHolder="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        label="Password"
+                    />
+                </CardSection>
                 <CardSection>
                     <Button>
                         Login!
