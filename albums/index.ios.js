13092bf636afd6cc2a3f46f048e953aea6e64597
Martin Runge <martinrunge@posteo.de>
2017-04-28 20:40:08 +0200

    some simple summary for albums app

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -6,6 +6,23 @@ import {AppRegistry, View, Button} from 'react-native';
 import {Header} from './src/components/Header';
 import {AlbumList} from './src/components/AlbumList';
 
+/**
+ * This app illustrate some react-native basics
+ *
+ * composition with components
+ *
+ * class based components with component level state and lifecycle methods
+ *
+ * functional components (mostly for presentation)
+ *
+ * usage of props to pass data from a parent to a child component
+ *
+ * usage of state to record and react to user input
+ *
+ * styling of components with flexbox
+ * 
+ */
+
 // Create a component
 const App = () => {
     return (
