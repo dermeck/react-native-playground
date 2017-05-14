6371b60d713795e0993e5f8c2b7d8f4b0e8b3c4c
Martin Runge <martinrunge@posteo.de>
2017-05-14 18:19:15 +0200

    added section headers

--- techstack/src/components/LibraryList.js
+++ techstack/src/components/LibraryList.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import { ListView } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { ListItem } from './ListItem';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
     componentWillMount () {
         const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
+            rowHasChanged: (r1, r2) => r1 !== r2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2
+
         });
 
-        this.dataSource = ds.cloneWithRows(this.props.libraries);
+        //this.dataSource = ds.cloneWithRows(this.props.libraries);
+        this.dataSource = ds.cloneWithRowsAndSections(this.getLibraryMap());
+        console.log(this.props.libraries);
+
     }
 
     renderRow(rowData) {
         return <ListItem data={ rowData }/>
+    }
+
+    renderSectionHeader(sectionData, category) {
+        let food = [
+            {name: "Lettuce", category: "Vegetable"},
+        {name: "Apple", category: "Fruit"},
+        {name: "Orange", category: "Fruit"},
+        {name: "Potato", category: "Vegetable"}
+    ];
+        console.log(food);
+        // console.log(sectionData);
+        // console.log(category);
+        return(
+
+            <View>
+                <Text>{category}</Text>
+            </View>
+        );
+    }
+
+    getLibraryMap () {
+        let map = {};
+        this.props.libraries.forEach((library) => {
+                if (!map[library.category]) {
+                    map[library.category] = [];
+                }
+                map[library.category].push(library);
+            }
+        );
+
+        return map;
     }
 
     render () {
@@ -21,6 +57,7 @@ class LibraryList extends Component {
             <ListView
                 dataSource={this.dataSource}
                 renderRow={this.renderRow}
+                renderSectionHeader={this.renderSectionHeader}
             />
         );
 
@@ -30,7 +67,6 @@ class LibraryList extends Component {
 // take global state object (application state)
 // provide part of it as props to the component
 const mapStateToProps = state => {
-    console.log(state);
     return { libraries: state.libraries};
 };
 
